d84dc8f58b7ad86be4667e14fbe324a3613f75e5
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-04-14 23:25:30 -0700

    Minor code refactoring

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
         if (e.keyCode === 82) {
             new Game()
         }
-    })
+    });
     addEventListener("keydown", function (e) { keysdown[e.keyCode] = true })
     addEventListener("keyup", function (e) { delete keysdown[e.keyCode] })
 })
